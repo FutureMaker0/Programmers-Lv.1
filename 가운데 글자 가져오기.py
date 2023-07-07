d65e7ebd72b905dbc18b4e4b19ffafa9730c4fc5
FutureMaker0 <120623320+FutureMaker0@users.noreply.github.com>
2023-07-07 21:11:34 +0900

    fc_0707

---
+++ 가운데 글자 가져오기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = ''
+    center = len(s) // 2
+    
+    if len(s) % 2 != 0:
+        answer += s[center]
+    else:
+        answer = s[center-1] + s[center]
+        
+    return answer
